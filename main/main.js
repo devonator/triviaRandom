9bce9a8113279a67f41236e029aa080552266875
Ryan Short <ryanshort.dev@gmail.com>
2024-09-18 19:03:02 -0400

    maintenence

--- main/main.js
+++ main/main.js
@@ -1,56 +1,70 @@
-document.querySelector('.getQuestion').addEventListener('click', getFetch);
-document.querySelector('.showAns').addEventListener('click', showAnswer)
+document.querySelector('.getQuestion').addEventListener('click', getFetch)
 
-
-function showAnswer(){
-  document.querySelector('ul').classList.toggle('hidden')
-}
 
 function getFetch(){
-  const url = `https://opentdb.com/api.php?amount=1&difficulty=easy`
+  const url = `https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple`
 
   fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data)
-      let questionContainer = document.querySelector('.displayQuestion')
-      let answerContainer = document.querySelector('.displayAnswer')
-      let categoryContainer = document.querySelector('span')
-      let question = data.results[0].question
-      .replaceAll('&quot;', '"')
-      .replaceAll("&#039;", "'")
-      .replaceAll('&amp;', '&') // characters & " ' do not render properly when pulled from the API. there must be a better way to to this. will come back to later
-      let answer = data.results[0].correct_answer
-      .replaceAll('&quot;', '"')
-      .replaceAll("&#039;", "'")
-      .replaceAll('&amp;', '&')
-      let category = data.results[0].category
-      let questionType = data.results[0].type
+      formatData(data)
+  })
+}
 
-      questionContainer.innerText = question
-      categoryContainer.innerText = `Category: ${category}`
+function formatData(data){
+  let question = data.results[0].question
+  let correctAnswers = data.results[0].correct_answer
+  let incorrectAnswers = data.results[0].incorrect_answers
+  let allAnswers = incorrectAnswers.concat(correctAnswers)
 
-      document.querySelector('ul').textContent = ''
-      const li = document.createElement('li')
-      li.textContent = answer
-      document.querySelector('ul').appendChild(li)
-        
+  // correctly formatting special characters in the 'question'
+  question = question
+    .replace(/&quot;/g, '"')
+    .replace(/&#039;/g, "'")
+    .replace(/&amp;/g, '&')
+    .replace(/&ouml;/g, 'ö')
 
-        // this part displays all incorrect answers. I'll come back to this later. I would like to style the correct answer green when the user clicks anywhere within the ul. maybe turn the incorrect answers red, too.
-        
-      // if(questionType === 'multiple'){
-      //   let multipleChoice = data.results[0].incorrect_answers
+  // Shuffle the allAnswers array 🔀
+  for (let i = allAnswers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [allAnswers[i], allAnswers[j]] = [allAnswers[j], allAnswers[i]];
+  }
 
-      //   multipleChoice.forEach(el => {
-      //     const li = document.createElement('li')
-      //     li.textContent = el
-      //     document.querySelector('ul').appendChild(li)
-      //   })
-      // }
-        
-      document.querySelector('ul').classList.add('hidden')
-      })
-      .catch(err => {
-          console.log(`error ${err}`);
-      });
+  displayEverything(question, correctAnswers, allAnswers)
+
+}
+
+
+function displayEverything(question, correctAnswer, answers){
+  let displayQuestionDiv = document.querySelector('.displayQuestion')
+  let displayAnswers = document.querySelector('.displayAnswers')
+  let correctOrNot = document.querySelector('.hideUntilAnswered')
+  
+  displayQuestionDiv.innerHTML = question
+  displayAnswers.textContent = '' // clear previous answers
+
+  answers.forEach(answer => {
+    answerParagraph = document.createElement('p')
+    answerParagraph.textContent = answer
+    displayAnswers.appendChild(answerParagraph)
+    answerParagraph.style = 'cursor: pointer;'
+
+    answerParagraph.addEventListener('click', function(){
+      if(this.textContent === correctAnswer){
+        alert('Correct!')
+        setTimeout(() => {
+          getFetch()
+        }, 2000)
+      }
+      else{
+        alert('Wrong!')
+        setTimeout(() => {
+          getFetch()
+        }, 2000)
+      }
+    })
+  })
+  console.log(correctAnswer)
+
 }
