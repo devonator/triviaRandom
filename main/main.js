639b091d0fe15e281cbd5ee88c029f621e7f38d9
Devon Conn <devonator99@gmail.com>
2024-09-21 01:13:13 -0400

    Init, Error Catching. and Animated Loading Screen
    catch method added to fetchData promise chain. error result, such as the 429 status, executes a setTimeout for 4 seconds, then runs the fetchData request again.
    
    object literal loadingScreen created. implemented in the getFetch function.
    
    init function created to setup the initialization of the script.

--- main/main.js
+++ main/main.js
@@ -1,29 +1,82 @@
-document.querySelector('.getQuestion').addEventListener('click', getFetch)
+const loadingScreen = {
+  element: document.querySelector(".loading-overlay"),
+  heading: null,
+  headingText: '',
+  colorInterval: null,
+
+  show(){
+    this.element.style.display = "flex";
+    this.colorize();
+  },
+
+  hide(){
+    this.element.style.display = "none";
+    clearInterval(this.colorInterval);
+    this.heading.innerHTML = '';
+    this.heading.textContent = this.headingText;
+  },
+  //Seizure Warning??
+  colorize(){
+    let headingTextLength = this.heading.textContent.length;
+    const colors = [' #39FF14', '#00FFFF', '#FFFF00', '#FF0000', '#FF33F6', '#FF00FF', '#FF8433', '#FFBD33'];
+    
+    function changeColor(letter) {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      letter.style.color = randomColor;
+    }
+
+    this.heading.innerHTML = '';
+
+    for(let i=0; i<headingTextLength; i++){
+      const spanElem = document.createElement('span');
+      spanElem.className="anim-letter";
+      spanElem.textContent = this.headingText[i];
+
+      this.heading.appendChild(spanElem);
+      
+      this.colorInterval = setInterval( () => changeColor(spanElem), 10);
+
+
+    }
+  },
+
+  init(){
+    this.heading = this.element.querySelector('h1');
+    this.headingText = this.heading.textContent;
+    this.hide();
+  }
+};
 
 
 function getFetch(){
   const url = `https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple`
 
-  fetch(url)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      console.log(data)
-      formatData(data)
-  })
+  loadingScreen.show();
+  var fetchData = function(){
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>formatData(data))
+    .then(() => loadingScreen.hide())
+    .catch(error=>setTimeout(fetchData, 4000));
+  };
+
+  fetchData();
 }
 
 function formatData(data){
-  let question = data.results[0].question
-  let correctAnswers = data.results[0].correct_answer
-  let incorrectAnswers = data.results[0].incorrect_answers
-  let allAnswers = incorrectAnswers.concat(correctAnswers)
+
+  let dataObj = data.results[0];
+  let question = dataObj.question;
+  let correctAnswers = dataObj.correct_answer;
+  let incorrectAnswers = dataObj.incorrect_answers;
+  let allAnswers = incorrectAnswers.concat(correctAnswers);
 
   // correctly formatting special characters in the 'question'
   question = question
     .replace(/&quot;/g, '"')
     .replace(/&#039;/g, "'")
     .replace(/&amp;/g, '&')
-    .replace(/&ouml;/g, 'ö')
+    .replace(/&ouml;/g, 'ö');
 
   // Shuffle the allAnswers array 🔀
   for (let i = allAnswers.length - 1; i > 0; i--) {
@@ -31,40 +84,45 @@ function formatData(data){
     [allAnswers[i], allAnswers[j]] = [allAnswers[j], allAnswers[i]];
   }
 
-  displayEverything(question, correctAnswers, allAnswers)
+  displayEverything(question, correctAnswers, allAnswers);
 
 }
 
 
 function displayEverything(question, correctAnswer, answers){
-  let displayQuestionDiv = document.querySelector('.displayQuestion')
-  let displayAnswers = document.querySelector('.displayAnswers')
-  let correctOrNot = document.querySelector('.hideUntilAnswered')
+  let displayQuestionDiv = document.querySelector('.displayQuestion');
+  let displayAnswers = document.querySelector('.displayAnswers');
+  let correctOrNot = document.querySelector('.hideUntilAnswered');
   
-  displayQuestionDiv.innerHTML = question
-  displayAnswers.textContent = '' // clear previous answers
+  displayQuestionDiv.innerHTML = question;
+  displayAnswers.textContent = ''; // clear previous answers
 
   answers.forEach(answer => {
-    answerParagraph = document.createElement('p')
-    answerParagraph.textContent = answer
-    displayAnswers.appendChild(answerParagraph)
-    answerParagraph.style = 'cursor: pointer;'
+    answerParagraph = document.createElement('p');
+    answerParagraph.textContent = answer;
+    displayAnswers.appendChild(answerParagraph);
+    answerParagraph.style = 'cursor: pointer;';
 
     answerParagraph.addEventListener('click', function(){
       if(this.textContent === correctAnswer){
-        alert('Correct!')
+        alert('Correct!');
         setTimeout(() => {
-          getFetch()
+          getFetch();
         }, 2000)
       }
       else{
-        alert('Wrong!')
+        alert('Wrong!');
         setTimeout(() => {
-          getFetch()
+          getFetch();
         }, 2000)
       }
     })
   })
-  console.log(correctAnswer)
 
 }
+
+function init(){
+  loadingScreen.init();
+  document.querySelector('.getQuestion').addEventListener('click', getFetch)
+}
+init();
